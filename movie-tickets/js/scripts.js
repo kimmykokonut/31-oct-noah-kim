0a3356f890042743e5e7a37938413e35e56801eb
noahkise <noah@kisefamily.com>
2023-10-31 14:56:11 -0700

    WIP
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- movie-tickets/js/scripts.js
+++ movie-tickets/js/scripts.js
@@ -15,9 +15,8 @@ const pawPatrol = new Movie("Paw Patrol: The Mighty Movie", "PG", "New-Release")
 const theRing = new Movie("The Ring", "PG13", "Second-Run");
 
 const moviesArray = [theShining, pawPatrol, theRing];
-console.log(moviesArray);
 
-let ratingsArray = ["R", "PG13", "PG"];
+let ratingsArray = ["R","PG13", "PG"];
 
 function assessAge(age) {
     if (age > 16) {
@@ -31,7 +30,10 @@ function assessAge(age) {
     }
 }
 
-function checkMovieAgainstRatingsArray(Movie) {
+function checkMovieAgainstRatingsArray(Movie, age) {
+    let ratingsArray = assessAge(age);
+    console.log(ratingsArray);
+    console.log(Movie.rating);
     if (ratingsArray.includes(Movie.rating)) {
         return ticketPrice();
     } else {
@@ -41,86 +43,86 @@ function checkMovieAgainstRatingsArray(Movie) {
 
 
 
-// function ticketPrice(age, time, newRelease) {
-//     let price = 15;
-//     if (age > 11 && age < 65) {
-//         if (time > 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 2;
-//                 return totalPrice;
-//             } else return price;
-//         } else if (time < 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 5;
-//                 return totalPrice;
-//             } else {
-//                 let totalPrice = price - 3;
-//                 return totalPrice;
-//             }
-//         }
-//     } else if (age > 64) {
-//         if (time > 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 5;
-//                 return totalPrice;
-//             } else {
-//                 let totalPrice = price - 3;
-//                 return totalPrice;
-//             }
-//         } else if (time < 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 8;
-//                 return totalPrice;
-//             } else {
-//                 let totalPrice = price - 6;
-//                 return totalPrice;
-//             }
-//         }
-//     } else {
-//         if (time > 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 7;
-//                 return totalPrice;
-//             } else {
-//                 let totalPrice = price - 5;
-//                 return totalPrice;
-//             }
-//         } else if (time < 5) {
-//             if (newRelease === false) {
-//                 let totalPrice = price - 10;
-//                 return totalPrice;
-//             } else {
-//                 let totalPrice = price - 8;
-//                 return totalPrice;
-//             }
-//         }
-//     }
-// }
-
-function findMovie(inputMovie) {
-    if (inputMovie === "The Shining") {
-        let objectMovie = theShining;
-        return objectMovie;
-    } else if (inputMovie === "The Ring") {
-        let objectMovie = theRing;
-        return objectMovie;
+function ticketPrice(age, time, newRelease) {
+    let price = 15;
+    if (age > 11 && age < 65) {
+        if (time > 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 2;
+                return totalPrice;
+            } else return price;
+        } else if (time < 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 5;
+                return totalPrice;
+            } else {
+                let totalPrice = price - 3;
+                return totalPrice;
+            }
+        }
+    } else if (age > 64) {
+        if (time > 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 5;
+                return totalPrice;
+            } else {
+                let totalPrice = price - 3;
+                return totalPrice;
+            }
+        } else if (time < 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 8;
+                return totalPrice;
+            } else {
+                let totalPrice = price - 6;
+                return totalPrice;
+            }
+        }
     } else {
-        let objectMovie = pawPatrol;
-        return objectMovie;
+        if (time > 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 7;
+                return totalPrice;
+            } else {
+                let totalPrice = price - 5;
+                return totalPrice;
+            }
+        } else if (time < 5) {
+            if (newRelease === false) {
+                let totalPrice = price - 10;
+                return totalPrice;
+            } else {
+                let totalPrice = price - 8;
+                return totalPrice;
+            }
+        }
     }
 }
 
 function handleFormSubmission(e) {
     e.preventDefault();
     const inputName = document.querySelector("input#nameInput").value;
-    const inputAge = parseInt(document.querySelector("input#ageInput")).value;
-    console.log(inputAge);
-    const inputMovie = document.getElementById("movieChoice").value;
-    const inputTime = document.getElementById("timeChoice").value;
-    findMovie(inputMovie);
-    let newTicketRequest = new TicketRequest(objectMovie, inputAge, inputTime);
-   
-    //int age and time
+    const inputAge = parseInt(document.querySelector("input#ageInput").value);
+    const inputMovie = document.querySelector("select#movieChoice").value;
+    function findMovie(inputMovie) {
+        if (inputMovie === "1") {
+            let objectMovie = theShining;
+            return objectMovie;
+        } else if (inputMovie === "2") {
+            let objectMovie = theRing;
+            return objectMovie;
+        } else {
+            let objectMovie = pawPatrol;
+            console.log(objectMovie);
+            return objectMovie;
+        }
+    }
+    const inputTime = parseInt(document.getElementById("timeChoice").value);
+    let chosenMovie = findMovie(inputMovie);
+    console.log(chosenMovie);
+    checkMovieAgainstRatingsArray(Movie, inputAge)    
+    let newTicketRequest = new TicketRequest(chosenMovie, inputAge, inputTime);
+
     //run ageAssess, which will error or cascade into checkMovieArray, error or cascase into ticketPrice
     //show ticket price
 
